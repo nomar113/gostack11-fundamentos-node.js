65c7bfccb16f343725a8ae0340068d7ec8c381db
Ramon Mesquita <ramonmesquita113@gmail.com>
2020-04-21 17:28:25 -0300

    Concluindo o desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,21 @@
 import Transaction from '../models/Transaction';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface Response {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -14,15 +26,52 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
-  public getBalance(): Balance {
-    // TODO
+  public getPartialBalance(type: 'income' | 'outcome'): number {
+    if (this.transactions.length > 0) {
+      const sum = this.transactions
+        .map(transaction => {
+          return transaction.type === type ? transaction.value : 0;
+        })
+        .reduce((total, value) => total + value);
+      return sum;
+    }
+    return 0;
   }
 
-  public create(): Transaction {
-    // TODO
+  public getBalance(): Response {
+    const income = this.getPartialBalance('income');
+    const outcome = this.getPartialBalance('outcome');
+    const total = income - outcome;
+
+    const response = {
+      transactions: this.transactions,
+      balance: {
+        income,
+        outcome,
+        total,
+      },
+    };
+
+    return response;
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const income = this.getPartialBalance('income') || 0;
+    const outcome = this.getPartialBalance('outcome') || 0;
+    const total = income - outcome;
+
+    if (type === 'outcome' && total - value < 0) {
+      throw Error('The outcome is greater than income');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
